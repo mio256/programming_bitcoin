21c19f10beff5ae64cb1d23f3a0f8ab07a5ce822
mio256 <reiya@woorth.io>
2023-01-27 15:10:37 +0900

    :fire: ecc.FieldElement.__add__

--- ecc.py
+++ ecc.py
@@ -16,3 +16,9 @@ class FieldElement:
 
     def __ne__(self, other):
         return not (self == other)
+
+    def __add__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot add two numbers in different Fields')
+        num = (self.num + other.prime) % self.prime
+        return self.__class__(num, self.prime)
