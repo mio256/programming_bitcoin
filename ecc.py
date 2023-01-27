ce4be6dfb1ee9263269da5e6e787941f1645feb7
mio256 <reiya@woorth.io>
2023-01-27 14:21:24 +0900

    :sparkles: ecc.py FieldElement

---
+++ ecc.py
@@ -0,0 +1,15 @@
+class FieldElement:
+
+    def __init__(self, num, prime):
+        if num >= prime or num < 0:
+            raise ValueError(f'Num {num} not in field range 0 to {prime-1}')
+        self.num = num
+        self.prime = prime
+
+    def __repr__(self):
+        return f'FieldElement_{self.prime}({self.num})'
+
+    def __eq__(self, other):
+        if other is None:
+            return False
+        return self.num == other.num and self.prime == other.prime
